0113b4352835bc4a68b267707d67d4450431e0bf
hywax <a.hywax@gmail.com>
2025-06-28 13:17:29 +0500

    feat(landing): restructure app layout and add index page with placeholder content

--- apps/landing/app/app.vue
+++ apps/landing/app/app.vue
@@ -1,18 +1,17 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <NuxtLoadingIndicator color="var(--ui-primary)" :height="2" />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
 
 <script setup lang="ts">
-useHead(() => ({
-  link: [
-    { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
-    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-    { rel: 'shortcut icon', href: '/favicon.ico' },
-    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-    { rel: 'manifest', href: '/site.webmanifest' },
-  ],
-}))
+const appConfig = useAppConfig()
+
+useSeoMeta({
+  titleTemplate: `%s - ${appConfig.name}`,
+})
 </script>
---
+++ apps/landing/app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <UIcon name="shotly:logo-symbol" class="size-8" />
+    <div class="text-xl mt-4">
+      Coming Soon
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+
+useSeoMeta({
+  title: `${appConfig.name} - ${appConfig.description}`,
+  titleTemplate: '%s',
+})
+</script>
