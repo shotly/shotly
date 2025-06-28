d7e29189b80fc987cc7b0ddf3e106f518deb9c9c
hywax <a.hywax@gmail.com>
2025-06-28 22:07:33 +0500

    feat(web): enhance app locale support by binding uiLocale to UApp component

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <UApp>
+  <UApp :locale="uiLocale">
     <NuxtLoadingIndicator color="var(--ui-primary)" :height="2" />
 
     <NuxtLayout>
@@ -9,7 +9,12 @@
 </template>
 
 <script setup lang="ts">
+import * as locales from '@nuxt/ui/locale'
+
+const { locale } = useI18n()
 const appConfig = useAppConfig()
+
+const uiLocale = computed(() => locales[locale.value])
 
 useSeoMeta({
   titleTemplate: `%s - ${appConfig.name}`,
