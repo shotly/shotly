01349d6cfb029db7317f9f6ae537eaec2cfaed3d
hywax <a.hywax@gmail.com>
2025-06-28 21:31:16 +0500

    feat(web): add initial pages for settings and main index with basic structure

---
+++ apps/web/app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+
+useSeoMeta({
+  title: `${appConfig.name} - ${appConfig.description}`,
+  titleTemplate: '%s',
+})
+</script>
