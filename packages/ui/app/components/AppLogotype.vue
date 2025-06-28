ff00398b6589aa718097e185792f35d2e8b4c06e
hywax <a.hywax@gmail.com>
2025-06-28 21:30:47 +0500

    feat(ui): add AppLogotype component with logo and app name

---
+++ packages/ui/app/components/AppLogotype.vue
@@ -0,0 +1,12 @@
+<template>
+  <NuxtLink :to="{ name: 'index' }" class="flex items-center gap-2 focus-visible:outline-primary">
+    <UIcon name="shotly:logo-symbol" class="size-5" />
+    <div class="text-lg font-medium">
+      {{ appConfig.name }}
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+</script>
