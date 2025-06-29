c9e51afc36eec02d77c4e6b744dcc63779e1e45b
hywax <a.hywax@gmail.com>
2025-06-29 14:34:18 +0500

    fix(ui): update AppLogotype component styles for logo size and text formatting

--- packages/ui/app/components/AppLogotype.vue
+++ packages/ui/app/components/AppLogotype.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink :to="{ name: 'index' }" class="flex items-center gap-2 focus-visible:outline-primary">
-    <UIcon name="shotly:logo-symbol" class="size-5" />
-    <div class="text-lg font-medium">
+    <UIcon name="shotly:logo-symbol" class="size-6" />
+    <div class="font-semibold text-base">
       {{ appConfig.name }}
     </div>
   </NuxtLink>
