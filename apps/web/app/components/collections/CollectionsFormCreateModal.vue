1bfd110736930f6d6e152d0b07198ecaa4c48e98
hywax <a.hywax@gmail.com>
2025-07-10 21:33:48 +0500

    feat: update CollectionsFormCreateModal with form fields and API integration for collection creation

--- apps/web/app/components/collections/CollectionsFormCreateModal.vue
+++ apps/web/app/components/collections/CollectionsFormCreateModal.vue
@@ -8,25 +8,78 @@
     }"
   >
     <template #body>
-      <UForm>
-        <UFormGroup :label="$t('collections.create.name')" name="name">
-          <UInput />
-        </UFormGroup>
+      <UForm
+        id="collections-form-create"
+        ref="form"
+        class="space-y-3"
+        :schema="schema"
+        :state="state"
+        @submit="onSubmit"
+      >
+        <div class="grid grid-cols-10 gap-3">
+          <UFormField name="icon" :label="$t('common.fields.icon')" class="col-span-2">
+            <USelect class="w-full" />
+          </UFormField>
+          <UFormField name="name" :label="$t('common.fields.name')" class="col-span-8">
+            <UInput v-model="state.name" class="w-full" />
+          </UFormField>
+        </div>
+        <UFormField name="description" :label="$t('common.fields.description')">
+          <UInput v-model="state.description" class="w-full" />
+        </UFormField>
+        <UFormField name="parentId" :label="$t('common.fields.parentCollection')">
+          ...
+        </UFormField>
       </UForm>
     </template>
 
     <template #footer>
-      <UButton :label="$t('common.actions.done')" @click="emit('close')" />
+      <UButton
+        form="collections-form-create"
+        :loading="form?.loading"
+        :label="$t('common.actions.create')"
+        @click="emit('close')"
+      />
     </template>
   </UModal>
 </template>
 
 <script setup lang="ts">
+import type { CollectionsCreatePayload as Schema, CUID } from '#shared/api'
+import type { FormSubmitEvent } from '@nuxt/ui'
+import { collectionsCreatePayloadSchema as schema } from '#shared/api'
+
 export interface CollectionsFormCreateModalEmits {
+  created: [id: CUID]
   close: []
 }
 
 const emit = defineEmits<CollectionsFormCreateModalEmits>()
 
+const { $api } = useNuxtApp()
 const { isCollectionsFormCreateModalOpen } = useApp()
+const form = useTemplateRef('form')
+const { state, reset } = useResettableReactive<Schema>(() => ({
+  name: '',
+  icon: '',
+  description: '',
+  parentId: '',
+}))
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  await $api('/api/collections', {
+    method: 'POST',
+    body: event.data,
+    onResponse: async ({ response }) => {
+      if (!response.ok) {
+        return
+      }
+
+      reset()
+      emit('created', response._data.id)
+
+      // todo: handle success message
+    },
+  })
+}
 </script>
