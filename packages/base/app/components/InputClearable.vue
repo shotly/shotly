38df11d2e63ad51b0148c41b8262c6325068b770
Alex <a.hywax@gmail.com>
2025-07-16 15:42:39 +0500

    feat: add collections description field (#93)

---
+++ packages/base/app/components/InputClearable.vue
@@ -0,0 +1,30 @@
+<template>
+  <UInput
+    :model-value="(modelValue as any)"
+    @update:model-value="$emit('update:modelValue', $event as any)"
+  >
+    <template v-if="modelValue" #trailing>
+      <ButtonClear
+        :model-value="(modelValue as any)"
+        :reset-value="resetValue"
+        @update:model-value="$emit('update:modelValue', $event as any)"
+      />
+    </template>
+  </UInput>
+</template>
+
+<script setup lang="ts">
+import type { ResetValue } from './ButtonClear.vue'
+
+export interface InputClearableProps {
+  modelValue?: string | number | null
+  resetValue?: ResetValue
+}
+
+interface InputClearableEmits {
+  'update:modelValue': [value: string | number | null]
+}
+
+defineProps<InputClearableProps>()
+defineEmits<InputClearableEmits>()
+</script>
