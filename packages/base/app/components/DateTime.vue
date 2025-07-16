3197b3ab49849100e7802d3cad603e6b5ae14f4d
hywax <a.hywax@gmail.com>
2025-07-16 15:51:08 +0500

    refactor: remove unused NuxtTimeProps interface from DateTime component

--- packages/base/app/components/DateTime.vue
+++ packages/base/app/components/DateTime.vue
@@ -3,37 +3,9 @@
 </template>
 
 <script setup lang="ts">
+import type { NuxtTimeProps } from '#app'
 import { NuxtTime } from '#components'
 import { useForwardProps } from 'reka-ui'
-
-// todo: https://github.com/shotly/shotly/issues/58
-export interface NuxtTimeProps {
-  locale?: string
-  datetime: string | number | Date
-  localeMatcher?: 'best fit' | 'lookup'
-  weekday?: 'long' | 'short' | 'narrow'
-  era?: 'long' | 'short' | 'narrow'
-  year?: 'numeric' | '2-digit'
-  month?: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow'
-  day?: 'numeric' | '2-digit'
-  hour?: 'numeric' | '2-digit'
-  minute?: 'numeric' | '2-digit'
-  second?: 'numeric' | '2-digit'
-  timeZoneName?: 'short' | 'long' | 'shortOffset' | 'longOffset' | 'shortGeneric' | 'longGeneric'
-  formatMatcher?: 'best fit' | 'basic'
-  hour12?: boolean
-  timeZone?: string
-
-  calendar?: string
-  dayPeriod?: 'narrow' | 'short' | 'long'
-  numberingSystem?: string
-
-  dateStyle?: 'full' | 'long' | 'medium' | 'short'
-  timeStyle?: 'full' | 'long' | 'medium' | 'short'
-  hourCycle?: 'h11' | 'h12' | 'h23' | 'h24'
-  relative?: boolean
-  title?: boolean | string
-}
 
 const props = defineProps<Omit<NuxtTimeProps, 'locale'>>()
 
